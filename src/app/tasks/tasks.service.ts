7169a8441e6754285c2463d017afbf9a665dbb7e
parkjunjae <wns1109@inplab.com>
2024-09-03 11:38:34 +0900

    로컬 스토리지에 저장하는법

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -28,6 +28,14 @@ export class TasksService {
         },
       ];
 
+      constructor() {
+        const tasks = localStorage.getItem('tasks');
+        
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
+      }
+
       getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
       }
@@ -39,10 +47,16 @@ export class TasksService {
             title: taskData.title,
             summary: taskData.summary,
             dueDate: taskData.date
-          })
+          });
+          this.saveTasks();
       }
 
       removeTask(id: string) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
+        this.saveTasks();
+      }
+
+      private saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
       }
 }
